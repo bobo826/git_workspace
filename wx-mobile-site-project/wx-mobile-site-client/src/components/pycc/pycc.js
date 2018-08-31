2ca7d29c4a780ff3b9298ae1458afe39d1d90d18
bobo826 <2696468332@qq.com>
2018-08-31 18:17:36 +0800

    地图导航完成检索结果列表下拉刷新

--- wx-mobile-site-project/wx-mobile-site-client/src/components/pycc/pycc.js
+++ wx-mobile-site-project/wx-mobile-site-client/src/components/pycc/pycc.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
-import { SearchBar } from 'antd-mobile';
+import { SearchBar ,List ,PullToRefresh, ListView } from 'antd-mobile';
+const Item = List.Item;
 import './pycc.css';
+
+
+const ListBody = (props) => {
+    return (
+      <div>
+        {props.children}
+      </div>
+    );
+}
+  
+
 export default class Pycc extends Component {
     constructor(props) {
         super(props);
+
+        const dataSource = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+        });
+
         this.state = {
-            value:'美食',//检索输入值
+            value:'停车场',//检索输入值
             point:{
                 lng:100.06792346,
                 lat:30.674414441
             },
-            search_point:[]
+            search_point:[],
+            dataSource,
+            height: document.documentElement.clientHeight,
+            useBodyScroll: false,
+        };
+        this.pageIndex = 0;
+    }
+
+    componentDidUpdate() {
+        if (this.state.useBodyScroll) {
+            document.body.style.overflow = 'auto';
         }
     }
 
     componentDidMount() {
+        
         //调用百度地图初始化并且完成相关空间配置
         this.map = new BMap.Map('map');
+        
         this.map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
         this.map.addControl(new BMap.GeolocationControl({
             anchor: BMAP_ANCHOR_BOTTOM_RIGHT,
@@ -49,22 +78,28 @@ export default class Pycc extends Component {
                 this.marker.addEventListener("click", (e) => {
                     console.log(this.state.point);
                 });
-
+               
                 //设置覆盖物
                 this.circle = new BMap.Circle(this.mPoint,1000,{fillColor:"blue", strokeWeight: 1 ,fillOpacity: 0.3, strokeOpacity: 0.3});
                 this.map.addOverlay(this.circle);
                 //使用地图检索功能配置检索范围及关键字
-                this.local =  new BMap.LocalSearch(this.map,{renderOptions: {map: this.map, autoViewport: false}}); 
+                this.local =  new BMap.LocalSearch(this.map,{renderOptions: {map: this.map, autoViewport: false }}); 
                 this.local.searchNearby(this.state.value,this.mPoint,1000);
+                // console.log(this.local.gotoPage(1));
+                
+                this.local.disableFirstResultSelection();//禁用自动选择第一个检索结果
+               
+                
                 //路线规划实例化
                 this.driving = new BMap.DrivingRoute(this.map, {renderOptions:{map: this.map, autoViewport: true}});
                 //设置添加标注后的回调函数。参数： pois: Array，通过marker属性可得到其对应的标注
                 this.local.setMarkersSetCallback(pois => {
                     for (const poi of pois) {
-                        // 为每个marker点添加事件，该事件用来规划路线
-                        poi.marker.addEventListener("click",e => {
+                            // 为每个marker点添加事件，该事件用来规划路线
+                            poi.marker.addEventListener("click",e => {
+                            //关闭默认信息窗口
+                            this.map.closeInfoWindow();
                             let endPoint = poi.marker.point;
-                            
                             //规划路线具体实现
                             this.driving.search(this.mPoint,endPoint);
                         });
@@ -73,37 +108,29 @@ export default class Pycc extends Component {
                 });
                 //检索提交事件
                 this.onSubmit.bind(this);
-                //自定义的标记marker的方法，暂时无法正常添加点击时间
-                // this.local.setSearchCompleteCallback(
-                //     (res) => {
-                //         console.log(res.Br);
-                //         for (const br of res.Br) {
-                            
-                //             this.setState({search_point:this.state.search_point.concat(br.point)});
-                //             //基于当前位置检索出来位置信息
-                //             this.rPoint = new BMap.Point(br.point.lng,br.point.lng)
-                //             //标记基于当前位置检索出来位置信息
-                //             console.log(this.rPoint);
-                //             this.rmarker = new BMap.Marker(this.rPoint);
-                //             this.map.addOverlay(this.rmarker); //在地图中添加marker
-                        
-                //             // this.rmarker.addEventListener("click", (e) => {
-                                
-                //             //     console.log(1);
-                //             // });
-
-                //         }
-                        
-
-                //     }
-                // );
-               
-                
+                //自定义的检索结果面板
             } else {
                 console.log('failed'+this.getStatus());
             }
         })
     }
+
+    
+
+    onRefresh() {
+        this.local.gotoPage(++this.pageIndex);
+        //console.log(this.local.getResults().Br);
+        let datas = this.local.getResults().Br;
+        console.log(datas);
+        this.setState({dataSource:this.state.dataSource.cloneWithRows(datas)});
+        this.row = (data) => {
+                return (
+                    <div>{data.title}</div>
+                );
+        }
+    }
+
+
 
     onSubmit (value) {
         //清楚上次检索信息
@@ -118,10 +145,11 @@ export default class Pycc extends Component {
         this.driving.clearResults();
         this.local.setMarkersSetCallback(pois => {
             for (const poi of pois) {
-                // 为每个marker点添加事件，该事件用来规划路线
-                poi.marker.addEventListener("click",e => {
+                    // 为每个marker点添加事件，该事件用来规划路线
+                    poi.marker.addEventListener("click",e => {
+                    //关闭默认信息窗口
+                    this.map.closeInfoWindow();
                     let endPoint = poi.marker.point;
-                    
                     //规划路线具体实现
                     this.driving.search(this.mPoint,endPoint);
                 });
@@ -134,8 +162,6 @@ export default class Pycc extends Component {
     onChange (value) {
         this.setState({ value:value });
     }
-
-
     render() {
         return (
             <div>
@@ -144,13 +170,30 @@ export default class Pycc extends Component {
                   placeholder="Search"
                   onSubmit={this.onSubmit.bind(this)}
                   onClear={value => console.log(value, 'onClear')}
-                  onFocus={() => console.log('onFocus')}
+                  onFocus={this.onRefresh.bind(this)}
                   onBlur={() => console.log('onBlur')}
                   onCancel={() => console.log('onCancel')}
                   showCancelButton
                   onChange={this.onChange.bind(this)}
                 />
-                <div id="map"></div>
+                <div id ="map"></div>
+                <div id = "res"></div>
+                <ListView
+                    renderBodyComponent={() => <ListBody />}
+                    key="0"
+                    dataSource={this.state.dataSource}
+                    renderRow={this.row}
+                    useBodyScroll={this.state.useBodyScroll}
+                    pullToRefresh={
+                        <PullToRefresh
+                            onRefresh={this.onRefresh.bind(this)}
+                        />
+                    }
+                    pageSize={10}
+                />
+
+              
+
             </div>
         )
     }
--- wx-mobile-site-project/wx-mobile-site-client/src/index.js
+++ wx-mobile-site-project/wx-mobile-site-client/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
+import { PullToRefresh, ListView, Button } from 'antd-mobile';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'antd-mobile/dist/antd-mobile.css';
 import '../src/index.css';
@@ -34,15 +35,188 @@ class App extends React.Component{
 
 // nav导航板块对应的子页面：
 //车辆防盗页面
-class Clfd extends Component {
-    render() {
-        return (
-            <div>
-                车辆防盗页面
-            </div>
-        )
+// class Clfd extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 车辆防盗页面
+//             </div>
+//         )
+//     }
+// }
+
+
+
+
+//数据来源
+const data = [
+    {
+  
+      des: '不是所有的兼职汪都需要风吹日晒'
+    },
+    {
+  
+      des: '不是所有的兼职汪都需要风吹日晒'
+    },
+    {
+  
+      des: '不是所有的兼职汪都需要风吹日晒'
+    },
+  ];
+  const NUM_ROWS = 8;//默认下拉刷新前页面显示几条信息
+  let pageIndex = 0;//页面索引
+  
+  function genData(pIndex = 0) {
+    const dataArr = [];
+    for (let i = 0; i < NUM_ROWS; i++) {
+      dataArr.push(`row - ${(pIndex * NUM_ROWS) + i}`);
     }
-}
+    console.log(dataArr);
+    return dataArr;
+  }
+  
+  class Clfd extends React.Component {
+    constructor(props) {
+      super(props);
+      const dataSource = new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      });
+  
+      this.state = {
+        dataSource,
+        refreshing: true,
+        isLoading: true,
+        height: document.documentElement.clientHeight,
+        useBodyScroll: false,
+      };
+    }
+ 
+  
+    componentDidUpdate() {
+      if (this.state.useBodyScroll) {
+        document.body.style.overflow = 'auto';
+      } else {
+        document.body.style.overflow = 'hidden';
+      }
+    }
+  
+    componentDidMount() {
+      const hei = this.state.height - ReactDOM.findDOMNode(this.lv).offsetTop;
+  
+      setTimeout(() => {
+        this.rData = genData();
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(genData()),
+          height: hei,
+          refreshing: false,
+          isLoading: false,
+        });
+      }, 1500);
+    }
+  
+    onRefresh ()  {
+      this.setState({ refreshing: true, isLoading: true });
+      // simulate initial Ajax
+      setTimeout(() => {
+        this.rData = genData();
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(this.rData),
+          refreshing: false,
+          isLoading: false,
+        });
+      }, 600);
+    };
+  
+    onEndReached  (event) {
+      // load new data
+      // hasMore: from backend data, indicates whether it is the last page, here is false
+      if (this.state.isLoading) {
+        return;
+      }
+      console.log('reach end', event);
+      this.setState({ isLoading: true });
+      setTimeout(() => {
+        this.rData = [...this.rData, ...genData(++pageIndex)];
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(this.rData),
+          isLoading: false,
+        });
+      }, 1000);
+    };
+  
+    render() {
+      const separator = (sectionID, rowID) => (
+        <div
+          key={`${sectionID}-${rowID}`}
+          style={{
+            backgroundColor: 'green',
+            height: 8,
+            borderTop: '1px solid #ECECED',
+            borderBottom: '1px solid #ECECED',
+          }}
+        />
+      );
+      let index = data.length - 1;
+      //list元信息
+      const row = (rowData, sectionID, rowID) => {
+        if (index < 0) {
+          index = data.length - 1;
+        }
+        const obj = data[index--];
+        return (
+          <div key={rowID}
+            style={{
+              padding: '0 15px',
+              backgroundColor: 'white',
+            }}
+          >
+            <div style={{ height: '50px', lineHeight: '50px', color: '#888', fontSize: '18px', borderBottom: '1px solid #ddd' }}>
+              {obj.des}
+            </div>
+          </div>
+        );
+      };
+  
+      
+      return (<div>
+        <Button
+          style={{ margin: '30px 15px' }}
+          inline
+          onClick={() => this.setState({ useBodyScroll: !this.state.useBodyScroll })}
+        >
+          {this.state.useBodyScroll ? 'useBodyScroll' : 'partial scroll'}
+        </Button>
+  
+  
+        <ListView
+          key={this.state.useBodyScroll ? '0' : '1'}
+          ref={el => this.lv = el}
+          dataSource={this.state.dataSource}
+          renderHeader={() => <span>Pull to refresh</span>}
+          renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+            {this.state.isLoading ? 'Loading...' : 'Loaded'}
+          </div>)}
+  
+          renderRow={row}
+          renderSeparator={separator}
+          useBodyScroll={this.state.useBodyScroll}
+          style={this.state.useBodyScroll ? {} : {
+            height: this.state.height,
+            border: '1px solid #ddd',
+            margin: '5px 0',
+          }}
+          pullToRefresh={<PullToRefresh
+            refreshing={this.state.refreshing}
+            onRefresh={this.onRefresh.bind(this)}
+          />}
+          onEndReached={this.onEndReached.bind(this)}
+          pageSize={5}
+        />
+      </div>);
+    }
+  }
+
+
 
 //月租续费页面
 class Yzxf extends Component {
@@ -83,8 +257,6 @@ class AppRoute extends Component {
         );
     }
 }
-
-
 
 ReactDOM.render(<AppRoute />, document.querySelector('body'));
 
